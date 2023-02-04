871d2310b370b66f1185e37f9eaa726ff176126b
LuisRoft <luisvelasco6541@gmail.com>
2023-02-03 22:23:41 -0500

    url added

--- app/src/main/java/com/example/examenlab/MainActivity.kt
+++ app/src/main/java/com/example/examenlab/MainActivity.kt
@@ -1,46 +1,55 @@
 package com.example.examenlab
 
 import android.os.Bundle
-import android.text.Editable
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.RequestBody.Companion.toRequestBody
 import com.example.examenlab.databinding.ActivityMainBinding
-
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
 
 
 class MainActivity() : AppCompatActivity() {
-
+    private val client = OkHttpClient()
+    private val url = "http://127.0.0.1:8000/usersPucem/"
     private lateinit var binding: ActivityMainBinding
-    private var client = OkHttpClient()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.buttonData.setOnClickListener {
-          end(binding.editTxtUsername.text, binding.editTxtName.text)
+          end(binding.editTxtUsername.text.toString(), binding.editTxtName.text.toString())
         }
 
     }
 
-    private fun end(username: Editable, name: Editable){
+    private fun end(username: String, name: String){
+
         val json = """
-            {
-                "username" : "$username",
-                "name" : $name"
-            }
-            """
+        {
+        "username": "$username",
+        "name": "$name"
+        }
+        """.trimIndent()
 
-        val requestBody = json.toRequestBody("application/json; charset=utf-8".toMediaType())
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val body = json.toRequestBody(mediaType)
 
-        val request = okhttp3.Request.Builder()
-            .url("https://www.example.com/api/login")
-            .post(requestBody)
+
+        val request = Request.Builder()
+            .url(url)
+            .post(body)
             .build()
 
-        val response = client.newCall(request).execute()
-        val responseBody = response.body?.string()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                    // Handle failure
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                Toast.makeText(this@MainActivity, "Please enter both the values", Toast.LENGTH_SHORT).show()            }
+        })
     }
 }
